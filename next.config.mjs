ed2768435cce2c761930e9afa73cbebbec23f148
Yago Martins <99210475+yagowill@users.noreply.github.com>
2026-03-19 15:32:06 -0300

    update: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  serverExternalPackages: ['next-mdx-remote'],
 }
 
 export default nextConfig
