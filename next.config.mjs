8869c3e9044ab398bafdc18d9ac808c1384c16f4
Yago Martins <99210475+yagowill@users.noreply.github.com>
2026-03-19 15:52:51 -0300

    Change serverExternalPackages to transpilePackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  serverExternalPackages: ['next-mdx-remote'],
+  transpilePackages: ['next-mdx-remote'],
 }
 
 export default nextConfig
